e8503093a3eb808ed6bec6e16f8dc2f4f6994e37
rshivaraman <r_shivaraman@yahoo.co.in>
2025-10-18 17:31:49 +0100

    Update players.js

--- pages/players.js
+++ pages/players.js
@@ -1,4 +1,4 @@
-import { supabase } from "../lib/supabase";
+import { supabase } from "../lib/supabaseClient";
 import { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
