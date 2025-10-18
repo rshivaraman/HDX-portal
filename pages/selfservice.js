743694ff836fa4b07a372e83826ea144a9dd5c63
rshivaraman <r_shivaraman@yahoo.co.in>
2025-10-18 17:32:25 +0100

    Update selfservice.js

--- pages/selfservice.js
+++ pages/selfservice.js
@@ -1,4 +1,4 @@
-import { supabase } from "../lib/supabase";
+import { supabase } from "../lib/supabaseClient";
 import { useState } from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
