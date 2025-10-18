81728b3fd9fc3ead212ddf159d1ad3d6356bcbf1
rshivaraman <r_shivaraman@yahoo.co.in>
2025-10-18 17:33:58 +0100

    Update layout.js

--- pages/layout.js
+++ pages/layout.js
@@ -1,18 +1,11 @@
 import '../styles/globals.css';
 import NavBar from '../components/NavBar';
 
-export const metadata = {
-  title: 'HDX Alliance Portal',
-  description: 'Alliance management system for Doomsday: Last Survivors',
-};
-
-export default function RootLayout({ children }) {
+export default function Layout({ children }) {
   return (
-    <html lang="en">
-      <body className="bg-gray-50 text-gray-900">
-        <NavBar />
-        <main className="p-4">{children}</main>
-      </body>
-    </html>
+    <>
+      <NavBar />
+      <main>{children}</main>
+    </>
   );
 }
